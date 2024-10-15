f04c81a5f7939d2523b972e824bf96a84560e8bb
unknown <shtorm20300@gmail.com>
2024-10-15 21:57:41 +0300

    добавили информацию о условных операторах и функциях

---
+++ users/eslaptenok/lessons/two_lesson.py
@@ -0,0 +1,29 @@
+# if 5 > 1 and 4 < 2:
+#     pass
+# elif 5 == 5:
+#     print(' ')
+# else:
+#     print('non')
+
+# increm = 0
+# while increm < 100:
+#     print (increm)
+#     increm += 1
+
+# numbers: list[int] = [1, 5, 7, -4]
+# for number in numbers:
+#     print(number)
+
+# for i in range(0, len(numbers), 1): # от, до, шаг
+#     print(numbers[i])
+
+# for num in numbers:
+#     if num % 2 == 0:
+#         print(num)
+
+def func_sum(x: int | float, y: int) -> int:
+    """Суммирует X и Y"""
+
+    return x + y
+
+print(func_sum(5.2, 2))

5c09c5765bbc70a021aa67f37eb4afb5efaf6f13
unknown <shtorm20300@gmail.com>
2024-10-15 21:14:53 +0300

    entering numbers

--- users/eslaptenok/homework/one.py
+++ users/eslaptenok/homework/one.py
@@ -1,3 +1,7 @@
 # Записать все, что мы прошли на первом занятии в заметки
 # Написать программу, которая принимает 2 числа(через input) и выводит их сумму
 # Найти как называть переменные(+ pep8)
+
+num1: int = int(input('Enter the first number: '))
+num2: int = int(input('Enter the second number: '))
+print(num1 + num2)

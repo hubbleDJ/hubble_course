284493992a7e0fa8468c1b20af7272456cd341b4
Настя <anastasiyazzva@gmail.com>
2024-10-23 09:02:42 +0300

    рассортировала по папкам

--- users/aazaitseva/homework/one.py
+++ users/aazaitseva/homework/one.py
@@ -1,3 +1,9 @@
 # Записать все, что мы прошли на первом занятии в заметки
 # Написать программу, которая принимает 2 числа(через input) и выводит их сумму
 # Найти как называть переменные
+
+text = 'Unbelievable'
+dict = {}
+for letter in text:
+	dict[letter] = text.count(letter)
+print(dict)
---
+++ users/aazaitseva/lesson/two_les.py
@@ -0,0 +1,30 @@
+if (1 == 2 and 3 < 5) or 5 == 7:
+    print("ghbdtn")
+elif 5 > 2:
+    pass
+else:
+    print("hello")
+
+var_list = [4, 985, 150] 
+if 2 not in var_list:
+    var_list.append(2)
+print(var_list)
+
+if  len("") == 0:
+    print(True)
+
+increment = 0
+while increment < 100: 
+    print(increment)
+    increment = increment + 1
+
+min_value = 0
+no_max_value = 100
+step = 1
+print(range(min_value, no_max_value, step))
+
+for value in var_list:
+    print(value)
+
+for symbol in "Hello":
+    print(symbol)

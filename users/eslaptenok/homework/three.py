cccf40fc12a680951c92a245d8099c8c56fb7017
unknown <shtorm20300@gmail.com>
2024-10-27 20:50:39 +0300

    hommework: recursion - correction 1

--- users/eslaptenok/homework/three.py
+++ users/eslaptenok/homework/three.py
@@ -2,15 +2,11 @@
 def factorial(num: int) -> int:
     """Подсчет факториала с помощью рекурсии"""
 
-    if num == 0 or num == 1:
+    if num in [0, 1]:
         return 1
     elif num < 0:
-        return 'the number must not be negative'
+        return -1
     return factorial(num - 1) * num
 
 print(factorial(5))
 print(factorial(-2))
-print(factorial(0))
-print(factorial(1))
-print(factorial(2))
-print(factorial(6))

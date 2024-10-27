9c8168fc4c8bb03b597872794a9a13bafddec8e0
unknown <shtorm20300@gmail.com>
2024-10-27 20:10:12 +0300

    homework: recursion

--- users/eslaptenok/homework/three.py
+++ users/eslaptenok/homework/three.py
@@ -1,1 +1,16 @@
 # Написать рекурсивную функцию факториала
+def factorial(num: int) -> int:
+    """Подсчет факториала с помощью рекурсии"""
+
+    if num == 0 or num == 1:
+        return 1
+    elif num < 0:
+        return 'the number must not be negative'
+    return factorial(num - 1) * num
+
+print(factorial(5))
+print(factorial(-2))
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(6))

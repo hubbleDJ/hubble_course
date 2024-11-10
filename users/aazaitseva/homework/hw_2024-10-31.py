395e81c1710608a015f3f8bc11411b9323355faa
Настя <anastasiyazzva@gmail.com>
2024-11-10 21:01:43 +0300

    bubble sort solution

--- users/aazaitseva/homework/hw_2024-10-31.py
+++ users/aazaitseva/homework/hw_2024-10-31.py
@@ -1,9 +1,19 @@
 #Написать пузырьковую сортировку
 
-def bubble_sort(sorted_list: list[int]) -> list[int]:
-    """сротирует список пузырьковой сортировкой"""
+#def bubble_sort(sorted_list: list[int]) -> list[int]:
+    #"""сротирует список пузырьковой сортировкой"""
 
-    return 
+    #return 
 
 #принцип пузырьковой сортировки
-var_list: list = [58, 5, 70]
+
+def bubble_sort(sorted_list: list[int]) -> list[int]:
+    n = len(sorted_list)
+    for i in range(n):
+        for y in range(0, n-i-1):
+            if sorted_list[y] > sorted_list[y+1]:
+                sorted_list[y], sorted_list[y+1] = sorted_list[y+1], sorted_list[y]
+    return sorted_list
+var_list = [58, 5, 458, 70, 9]
+sort_list = bubble_sort(var_list)
+print(sort_list)

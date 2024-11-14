72d230d1d56a0d7e5416a30a896e9063157bf94f
Настя <anastasiyazzva@gmail.com>
2024-11-14 21:51:05 +0300

    add new chages and deleted comments

--- users/aazaitseva/homework/hw_2024-10-31.py
+++ users/aazaitseva/homework/hw_2024-10-31.py
@@ -1,18 +1,11 @@
-#Написать пузырьковую сортировку
-
-#def bubble_sort(sorted_list: list[int]) -> list[int]:
-    #"""сротирует список пузырьковой сортировкой"""
-
-    #return 
-
-#принцип пузырьковой сортировки
-
 def bubble_sort(sorted_list: list[int]) -> list[int]:
     n = len(sorted_list)
+    sort = False
     for i in range(n):
         for y in range(0, n-1):
             if sorted_list[y] > sorted_list[y+1]:
                 sorted_list[y], sorted_list[y+1] = sorted_list[y+1], sorted_list[y]
+                sort = True
     return sorted_list
 var_list = [58, 5, 458, 70, 9]
 sort_list = bubble_sort(var_list)

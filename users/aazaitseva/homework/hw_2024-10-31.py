ce754aebe70324bcc67dfe648dfc65cc566ac5f1
Настя <anastasiyazzva@gmail.com>
2024-11-16 12:29:51 +0300

    correct hw

--- users/aazaitseva/homework/hw_2024-10-31.py
+++ users/aazaitseva/homework/hw_2024-10-31.py
@@ -1,12 +1,15 @@
 def bubble_sort(sorted_list: list[int]) -> list[int]:
-    n = len(sorted_list)
-    sort = False
-    for i in range(n):
-        for y in range(0, n-1):
+    len_sorted_list = len(sorted_list)
+    for i in range(len_sorted_list):
+        sorted_flag = False
+        for y in range(0, len_sorted_list-1):
             if sorted_list[y] > sorted_list[y+1]:
                 sorted_list[y], sorted_list[y+1] = sorted_list[y+1], sorted_list[y]
-                sort = True
+                sorted_flag = True
+        if not sorted_flag:
+            break
+        print(sorted_list)
     return sorted_list
-var_list = [58, 5, 458, 70, 9]
+var_list = [5, 58, 458, 70, 9]
 sort_list = bubble_sort(var_list)
 print(sort_list)

0fe0998cc6cd173c4b6f77e5c8dd9ce25ea100fb
Настя <anastasiyazzva@gmail.com>
2024-12-02 18:14:30 +0300

    correct hw_2024-11-16

--- users/aazaitseva/homework/hw_2024-11-16.py
+++ users/aazaitseva/homework/hw_2024-11-16.py
@@ -1,8 +1,12 @@
-#написать рекурсивну. ф-ю подсчета факториала
-def factorial(n):
-    if n == 1 or n == 0:
+def factorial(n: int) -> int: 
+    """вычисляет факториал числа"""
+
+    if n in [0, 1]:
         return 1
+    elif n < 0:
+        return 0
     else:
         return n * factorial(n-1)
+
 n = int(input("Ввод числа:"))
 print(factorial(n))

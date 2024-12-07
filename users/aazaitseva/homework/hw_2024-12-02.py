04ff440acc71147149f4ead1f9d9b55458f33aab
Maksim Strometskiy <mvstrometskiy@avito.ru>
2024-12-07 21:55:50 +0300

    create hw_2024-12-02.py

---
+++ users/aazaitseva/homework/hw_2024-12-02.py
@@ -0,0 +1,55 @@
+# Нужно заполнить тела функций, согласно комментариев, внутри функции
+# Комментарии не менять и не удалять
+
+def maxNumList(numbers: list[int]) -> int:
+    """
+        Находит максимальное значение в списке
+        Использовать функцию max нельзя
+        
+        Пример: maxNumList([0, -1, 19999, -80898, 898]) вернет 19999
+    """
+    
+    maxNum = None
+    return maxNum
+
+def minNumList(numbers: list[int]) -> int:
+    """
+        Находит минимальное значение в списке
+        Использовать функцию min нельзя
+        
+        Пример: minNumList([0, -1, 19999, -80898, 898]) вернет -80898
+    """
+    
+    minNum = None
+    return minNum
+
+def sumNumbersInList(numbers: list[int]) -> int:
+    """
+        Находит сумму значений в списке
+        Использовать функцию sum нельзя
+        
+        Пример: sumNumbersInList([0, -1, 19999, -80898, 898]) вернет -60002
+    """
+    
+    sumNum = None
+    return sumNum
+
+def getChetNumsInList(numbers: list[int]) -> list[int]:
+    """
+        Возвращает все четные числа в списке
+        
+        Пример: getChetNumsInList([0, -1, 19999, -80898, 898]) вернет [-80898, 898]
+    """
+    
+    chetNums = []
+    return chetNums
+
+def getCountNumsMoreNum(numbers: list[int], num: int) -> int:
+    """
+        Возвращает количество чисел в списке, которые больше, чем num
+        
+        Пример: getCountNumsMoreNum([0, -1, 19999, -80898, 898], 19) вернет 2
+    """
+    
+    countNumsMoreNum = 0
+    return countNumsMoreNum

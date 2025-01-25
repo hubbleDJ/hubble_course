e142c5e0e36ed1df01b649ba5ecd51159d35bc84
agubanova <an.gubanova2012@yandex.ru>
2025-01-25 18:13:26 +0500

    second try

---
+++ group/homework/agubanova/2025_01_22_part1.py
@@ -0,0 +1,9 @@
+dig_var1: int = int(input('Напишите первое число: '))
+dig_var2: int = int(input('Напишите второе число: '))
+
+print(f'{dig_var1} + {dig_var2} = {dig_var1 + dig_var2}')
+print(f'{dig_var1} - {dig_var2} = {dig_var1 - dig_var2}')
+print(f'{dig_var1} * {dig_var2} = {dig_var1 * dig_var2}')
+print(f'{dig_var1} / {dig_var2} = {dig_var1 / dig_var2}')
+print(f'{dig_var1} % {dig_var2} = {dig_var1 % dig_var2}')
+print(f'{dig_var1} // {dig_var2} = {dig_var1 // dig_var2}')

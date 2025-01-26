94a3a3e9f79f7f9dc3f16ea21616c82614125b9e
Jkrsh <krasheninnikova-y05@mail.ru>
2025-01-26 22:41:52 +0600

    update

--- group/homework/jekrasheninnikova/2025_01_22_part2.py
+++ group/homework/jekrasheninnikova/2025_01_22_part2.py
@@ -1,7 +1,9 @@
-myData: dict = {}
-myData['name'] = input(f'Как тебя зовут?\n')
-myData['gender'] = input(f'Какой у тебя пол? Женский/Мужской\n')
-myData['age'] = input(f'Сколько тебе лет?\n')
-myData['hobby'] = input(f'Какие у тебя хобби? Введи три наиболее значимых для тебя\n'), input(), input()
-myData['music'] = input(f'Какая у тебя любимая музыка? Введи два наиболее интересных для тебя жанра/прилагательных\n'), input()
-print(f'''Привет, {myData['name']}, возраст которой {myData['age']} лет и пол {myData['gender']}, любящей {myData['hobby'][0]}, {myData['hobby'][1]} и {myData['hobby'][2]}, слушающая {myData['music'][0]} и {myData['music'][1]}''')
+userinfo: dict = {}
+userinfo['name'] = input(f'Как тебя зовут?\n')
+userinfo['gender'] = input(f'Какой у тебя пол? Женский/Мужской\n')
+userinfo['age'] = input(f'Сколько тебе лет?\n')
+userinfo['hobby'] = input(f'Какие у тебя хобби? Введи три наиболее значимых для тебя\n'), input(), input()
+userinfo['music'] = input(f'Какая у тебя любимая музыка? Введи два наиболее интересных для тебя жанра/прилагательных\n'), input()
+print(f'''Привет, {userinfo['name']}, возраст которой {userinfo['age']} лет и пол {userinfo['gender']},' \n
+любящая {userinfo['hobby'][0]}, {userinfo['hobby'][1]} и {userinfo['hobby'][2]},' \n
+слушающая {userinfo['music'][0]} и {userinfo['music'][1]}'\n''')

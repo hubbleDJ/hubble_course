917f2f2b69a967e00fe63ea291e7dab48d410ba7
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-01-26 20:53:58 +0300

    homework 2025_01_22

--- group/homework/jekrasheninnikova/2025_01_22_part2.py
+++
@@ -1,9 +0,0 @@
-userinfo: dict = {}
-userinfo['name'] = input(f'Как тебя зовут?\n')
-userinfo['gender'] = input(f'Какой у тебя пол? Женский/Мужской\n')
-userinfo['age'] = input(f'Сколько тебе лет?\n')
-userinfo['hobby'] = input(f'Какие у тебя хобби? Введи три наиболее значимых для тебя\n'), input(), input()
-userinfo['music'] = input(f'Какая у тебя любимая музыка? Введи два наиболее интересных для тебя жанра/прилагательных\n'), input()
-print(f'''Привет, {userinfo['name']}, возраст которой {userinfo['age']} лет и пол {userinfo['gender']},' \n
-любящая {userinfo['hobby'][0]}, {userinfo['hobby'][1]} и {userinfo['hobby'][2]},' \n
-слушающая {userinfo['music'][0]} и {userinfo['music'][1]}'\n''')
--- group/homework/jekrasheninnikova/hw_2025_01_22_part1.py
+++ group/homework/jekrasheninnikova/hw_2025_01_22_part1.py
@@ -1,3 +1,5 @@
+"""Homework 2025-01-22 part 1"""
+
 x: float = float(input(f'Введите первое число\n'))
 y: float = float(input(f'Введите второе число\n'))
 

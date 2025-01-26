805e55d2263a73b8e2032133cf191dae6100ee2a
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-01-26 22:50:26 +0300

    rename my files homework

--- group/homework/jekrasheninnikova/hw_2025_01_22_part1.py
+++ group/homework/jekrasheninnikova/hw_2025_01_22_part1.py
@@ -1,7 +1,7 @@
 """Homework 2025-01-22 part 1"""
 
-x: float = float(input(f'Введите первое число\n'))
-y: float = float(input(f'Введите второе число\n'))
+x: float = float(input('Введите первое число\n'))
+y: float = float(input('Введите второе число\n'))
 
 print(f'{x} + {y} = {x + y}')
 print(f'{x} - {y} = {x - y}')
--- users/aazaitseva/lesson/hw_2024_12_14.py
+++ users/aazaitseva/lesson/hw_2024_12_14.py
@@ -34,3 +34,4 @@ async def main():
     await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == '__main__':
+    main()

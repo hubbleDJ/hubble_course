9b21ce1be40624a212f96609fd9c12ab54a33863
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-01-26 20:11:09 +0300

    fix code with linter. part 1

---
+++ group/homework/agubanova/hw_2025_01_15.py
@@ -0,0 +1,5 @@
+"""First homework"""
+
+if __name__ == "__main__":
+    print("Hello world!")
+
--- group/homework/agubanova/hw_2025_01_22_part1.py
+++ group/homework/agubanova/hw_2025_01_22_part1.py
@@ -1,3 +1,5 @@
+"""Homework 2025-01-22 part 1"""
+
 dig_var1: int = int(input('Напишите первое число: '))
 dig_var2: int = int(input('Напишите второе число: '))
 
@@ -7,3 +9,4 @@ print(f'{dig_var1} * {dig_var2} = {dig_var1 * dig_var2}')
 print(f'{dig_var1} / {dig_var2} = {dig_var1 / dig_var2}')
 print(f'{dig_var1} % {dig_var2} = {dig_var1 % dig_var2}')
 print(f'{dig_var1} // {dig_var2} = {dig_var1 // dig_var2}')
+

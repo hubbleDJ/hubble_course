9791793f98173030d9c4dbe06d60cf9e6c5cd728
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-01-26 20:31:00 +0300

    fix code with linter. part 4

--- group/homework/daleshin/hw_2025_01_22_part1.py
+++ group/homework/daleshin/hw_2025_01_22_part1.py
@@ -1,3 +1,5 @@
+"""Homework 2025-01-22 part 1"""
+
 digit1: int = int(input('Введите число 1: '))
 digit2: int = int(input('Введите число 2: '))
 
@@ -10,7 +12,3 @@ else:
     print(f'{digit1} / {digit2} = {digit1 / digit2}') # тут нужна проверка на то что делим не на 0
     print(f'{digit1} % {digit2} = {digit1 % digit2}') # тут нужна проверка на то что делим не на 0
     print(f'{digit1} // {digit2} = {digit1 // digit2}') # тут нужна проверка на то что делим не на 0
-
-# oper = ['+', '-', '*', '/', '%', '//']
-# for i in oper:
-    #print(f'{digit1} {oper[i]} {digit2} = {digit1 тут не прилумал как заставить из списка вытягивать операции oper[i] digit2}')
--- group/homework/daleshin/hw_2025_01_22_part2.py
+++ group/homework/daleshin/hw_2025_01_22_part2.py
@@ -1,3 +1,5 @@
+"""Homework 2025-01-22 part 2"""
+
 dataset: dict = {
     "name": "",
     "surname": "",
@@ -9,12 +11,15 @@ dataset: dict = {
 
 dataset["name"] = input('Напишите ваше имя: ')
 dataset["surname"] = input('Напишите вашу фамилию: ')
-dataset["age"] = input('Сколько вам лет: ') # не знаю как обыргать int, поэтому убрал, предполагая , что можно написать "Двадцать" или что-то типо того
+dataset["age"] = input('Сколько вам лет: ')
 dataset["sex"] = input('Укажите ваш пол: ')
 dataset["hobby"] = input('Напишите, чем вы увлекаетесь: ')
 dataset["city"] = input('Напишите, где вы живете: ')
 
-print(f'Имя {dataset.get("name", "Пользователь не указал имя")}, а фамилия {dataset.get("surname", "Пользователь не указал фамилию")}')
+print(
+    f'Имя {dataset.get("name", "Пользователь не указал имя")}, '
+    f'а фамилия {dataset.get("surname", "Пользователь не указал фамилию")}'
+)
 print(f'Вот столько лет от роду: {dataset.get("age", "Пользователь не указал возраст")}')
 print(f'Гендер: {dataset.get("sex", "Пользователь не указал пол")}')
 print(f'Увлечения: {dataset.get("hobby", "Пользователь не указал хобби")}')

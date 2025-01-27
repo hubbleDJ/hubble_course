73c68bba64cdeb1077aa0573dcfa277ef13b9add
festo24 <festo1992@gmail.com>
2025-01-28 01:33:36 +0600

    fixed(0.1)

--- group/homework/uvarovfesto24/2025_01_22_part1.py
+++ group/homework/uvarovfesto24/2025_01_22_part1.py
@@ -9,23 +9,7 @@ print(str(x)+ ' / '+ str(y), ' = ', int(x)/int(y))
 print(str(x)+ ' % '+ str(y), ' = ', int(x)%int(y))
 print(str(x)+ ' // '+ str(y), ' = ', int(x)//int(y))
 
-    #мой первый вариант написания
-# print("x + y= ", end="")
-# print(int(x) + int(y))
-# print("x - y= ", end="")
-# print(int(x)-int(y))
-# print("x * y= ", end="")
-# print(int(x)*int(y))
-# print("x / y= ", end="")
-# print(int(x)/int(y))
-# print("x % y= ", end="")
-# print(int(x)%int(y))
-# print("x // y= ", end="")
-# print(int(x)//int(y))
-   
-    #мой правильный правильный
-#print(x ,'+' ,y  ,'=', end="")
-#print(int(x) + int(y))
+
 
 
 
--- group/homework/uvarovfesto24/2025_01_22_part2.py
+++ group/homework/uvarovfesto24/2025_01_22_part2.py
@@ -3,19 +3,17 @@
     # По итогу должен получиться dict, из которого вы генерируете строку с приветствием (Привет {name}, любящий {hobby[0]} и т д)
     # Выводим строку в консоль
    
-firstDictVar: dict = {
-'name' : ' Konstantin',
-'surname' : ' Uvarov',
-"where'a you?" : " jvcr",
-"what time is't now?" : " 6 am",
-"ты спишь?" : " no sleep",
-}
-print("Здравствуй путник, я расскажу тебе историю, но для начала познакомимся")
-firstDictVar['name']= input("Звать тя как? ")
-firstDictVar['surname']= input("А Фамилия твоя? ")
-firstDictVar["where'a you?"]= input("откуда ж ты? ")
-firstDictVar["what time is't now?"]= input("Время не подскажишь? ")
-firstDictVar['ты спишь?']= input("спишь/не спишь? ")
+firstDictVar: dict = {}
 
-print(f''' {firstDictVar['name']} {firstDictVar['surname']} из города {firstDictVar["where'a you?"]}''', end="" 
-f''' был в состоянии {firstDictVar['ты спишь?']} до {firstDictVar["what time is't now?"]} и решил вызвать ктулху, если вы это видите, значит у него всё получилось''')
+print("Здравствуй путник, я расскажу тебе историю, но для начала познакомимся")
+firstDictVar['name'] = input("Звать тя как? ")
+firstDictVar['surname'] = input("А Фамилия твоя? ")
+firstDictVar["where_from"] = input("откуда ж ты? ")
+firstDictVar["time__now"] = input("Время не подскажишь? ")
+firstDictVar['sleep'] = input("спишь/не спишь? ")
+
+print(f'''   {firstDictVar['name']} {firstDictVar['surname']}  
+    из города {firstDictVar["where_from"]}''', end="" 
+    f''' был в состоянии {firstDictVar['sleep']} 
+    до {firstDictVar["time__now"]} и решил вызвать ктулху,
+    если вы это видите, значит у него всё получилось''')

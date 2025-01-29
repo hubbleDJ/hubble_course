acef7357942021e9775a1d5d2d7acb7247525243
Jkrsh <krasheninnikova-y05@mail.ru>
2025-01-30 02:23:48 +0600

    Change and resave syntax mistakes

--- group/homework/jekrasheninnikova/2025_01_22_part2.py
+++ group/homework/jekrasheninnikova/2025_01_22_part2.py
@@ -1,9 +1,13 @@
-userinfo: dict = {}
-userinfo['name'] = input(f'Как тебя зовут?\n')
-userinfo['gender'] = input(f'Какой у тебя пол? Женский/Мужской\n')
-userinfo['age'] = input(f'Сколько тебе лет?\n')
-userinfo['hobby'] = input(f'Какие у тебя хобби? Введи три наиболее значимых для тебя\n'), input(), input()
-userinfo['music'] = input(f'Какая у тебя любимая музыка? Введи два наиболее интересных для тебя жанра/прилагательных\n'), input()
-print(f'''Привет, {userinfo['name']}, возраст которой {userinfo['age']} лет и пол {userinfo['gender']},' \n
-любящая {userinfo['hobby'][0]}, {userinfo['hobby'][1]} и {userinfo['hobby'][2]},' \n
-слушающая {userinfo['music'][0]} и {userinfo['music'][1]}'\n''')
+userInfo: dict = {}
+userInfo['name'] = input(f'Как тебя зовут?\n')
+userInfo['gender'] = input(f'Какой у тебя пол? Женский/Мужской\n')
+userInfo['age'] = input(f'Сколько тебе лет?\n')
+userInfo['hobby'] = input(f'Какие у тебя хобби? Введи три наиболее значимых для тебя\n'), input(), input()
+userInfo['music'] = input(f'Какая у тебя любимая музыка? Введи два наиболее интересных для тебя жанра/прилагательных\n'), input()
+print(
+    f'''Привет, {userInfo['name']},'''
+    f''' возраст которой {userInfo['age']} лет '''
+    f'''и пол {userInfo['gender']},' \n'''
+    f'''любящая {userInfo['hobby'][0]}, {userInfo['hobby'][1]} и {userInfo['hobby'][2]},' \n'''
+    f'''слушающая {userInfo['music'][0]} и {userInfo['music'][1]}' '''
+)

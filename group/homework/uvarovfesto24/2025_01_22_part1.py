19df6e9f3fb04fcb01a0b6a11811eff3f73f527c
festo24 <festo1992@gmail.com>
2025-01-30 17:08:27 +0600

    edit part1: del "str" and str

--- group/homework/uvarovfesto24/2025_01_22_part1.py
+++ group/homework/uvarovfesto24/2025_01_22_part1.py
@@ -1,16 +1,10 @@
-x: int = input("x= ")
-y: int = input("y= ")
+x: str = input("x= ")
+y: str = input("y= ")
 
     #Правильный вариант
-print(str(x)+ ' + '+ str(y), ' = ', int(x)+int(y))
+print(x + ' + ' + y , ' = ', int(x)+int(y))
 print(f'{x} - {y} = {int(x)-int(y)}')
 print('{} * {} = {}'.format(x, y, int(x)*int(y)))
-print(str(x)+ ' / '+ str(y), ' = ', int(x)/int(y))
-print(str(x)+ ' % '+ str(y), ' = ', int(x)%int(y))
-print(str(x)+ ' // '+ str(y), ' = ', int(x)//int(y))
-
-
-
-
-
-
+print(x + ' / ' + y, ' = ', int(x)/int(y))
+print(x + ' % ' + y, ' = ', int(x)%int(y))
+print(x + ' // '+ y, ' = ', int(x)//int(y))

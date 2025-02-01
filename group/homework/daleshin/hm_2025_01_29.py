fea5d310feecae009c1cc8937f38979d53910b51
AlDenis93 <Ghjuhfvvbhe.2021!>
2025-02-01 18:16:36 +0500

    edited hw

--- group/homework/daleshin/hm_2025_01_29.py
+++ group/homework/daleshin/hm_2025_01_29.py
@@ -1,7 +1,9 @@
 test_nums: list[int]  = [12, 32, 0, -4, 4, 564564, 63, 5976]
 # test_nums.sort()
 
-for k in range(len(test_nums) - 1): # отнимаю 1, чтобы ограничить длину списка, так как у меня дальше есть выражение k+1 , при последжних итерациях выдает ошибку IndexError: list index out of range , потому что ссылается на несуществующий элемент списка
-    if test_nums[k] > test_nums[k + 1]:
-        test_nums[k], test_nums[k + 1] = test_nums[k + 1], test_nums[k] # меняю местами в списке значения
+for i in range(len(test_nums) - 1):
+    for k in range(len(test_nums) - 1): # отнимаю 1, чтобы ограничить длину списка, так как у меня дальше есть выражение k+1 , при последжних итерациях выдает ошибку IndexError: list index out of range , потому что ссылается на несуществующий элемент списка
+        if test_nums[k] > test_nums[k + 1]:
+            test_nums[k], test_nums[k + 1] = test_nums[k + 1], test_nums[k] # меняю местами в списке значения
+
 print(test_nums)

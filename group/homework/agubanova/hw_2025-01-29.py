dae6b7b6a932886e37f537f5d8ea3831bdef60ba
agubanova <an.gubanova2012@yandex.ru>
2025-02-01 16:27:36 +0300

    change my hw

--- group/homework/agubanova/hw_2025-01-29.py
+++ group/homework/agubanova/hw_2025-01-29.py
@@ -1,14 +1,8 @@
 # Написать пузырьковую сортровку
 
-# [12, 32, 0, -4, 4, 564564]
-# [12, 0, 32, -4, 4, 564564]
-# [12, 0, -4, 32, 4, 564564]
-# [12, 0, -4, 4, 32, 564564]
-# [12, 0, -4, 4, 32, 564564]
-
 spisok_num: list = [12, 32, 0, -4, 4, 564564]
-for i in range(len(spisok_num) - 1):
-    for k in range(len(spisok_num) - i - 1):
-        if spisok_num[k] > spisok_num[k + 1]:
-            spisok_num[k], spisok_num[k + 1] = spisok_num[k + 1], spisok_num[k]
+for pass_num in range(len(spisok_num) - 1):
+    for compare_index in range(len(spisok_num) - pass_num - 1):
+        if spisok_num[compare_index] > spisok_num[compare_index + 1]:
+            spisok_num[compare_index], spisok_num[compare_index + 1] = spisok_num[compare_index + 1], spisok_num[compare_index]
 print(spisok_num)

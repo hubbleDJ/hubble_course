1b8a52c734853e18c0e5dbcd5e0d6c381350017b
AlDenis93 <Ghjuhfvvbhe.2021!>
2025-02-01 18:45:48 +0500

    edited hw

--- group/homework/daleshin/hm_2025_01_29.py
+++ group/homework/daleshin/hm_2025_01_29.py
@@ -2,8 +2,8 @@ test_nums: list[int]  = [12, 32, 0, -4, 4, 564564, 63, 5976]
 # test_nums.sort()
 
 for _ in range(len(test_nums) - 1):
-    for comparator in range(len(test_nums) - 1): # отнимаю 1, чтобы не ссылаться на неусуществующий индеск 
-            if test_nums[comparator] > test_nums[comparator + 1]:
-                test_nums[comparator], test_nums[comparator + 1] = test_nums[comparator + 1], test_nums[comparator] # меняю местами в списке значения
+    for element_index in range(len(test_nums) - 1): # отнимаю 1, чтобы не ссылаться на неусуществующий индеск 
+            if test_nums[element_index] > test_nums[element_index + 1]:
+                test_nums[element_index], test_nums[element_index + 1] = test_nums[element_index + 1], test_nums[element_index] # меняю местами в списке значения
 
 print(test_nums)

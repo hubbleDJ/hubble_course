83fa2bb30fb3765f25b7339dc60a20080ed76c5d
Jkrsh <krasheninnikova-y05@mail.ru>
2025-02-02 01:42:59 +0600

    created hw_2025-01-29

--- group/homework/jekrasheninnikova/2025_01_29.py
+++
@@ -1,9 +0,0 @@
-puzirek_method: list = [12, 32, 0, -4, 4, 564564]
-
-print(f'Исходные данные: {puzirek_method}')
-
-for i in range(len(puzirek_method)):
-    for j in range (len(puzirek_method)-1):
-        if puzirek_method[j]>puzirek_method[j + 1]:
-            puzirek_method[j], puzirek_method[j + 1] = puzirek_method[j + 1], puzirek_method[j]
-            print(puzirek_method)

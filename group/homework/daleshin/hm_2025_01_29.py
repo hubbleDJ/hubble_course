60939c3d45fda071edf3a7d832fa6ea878833c2f
AlDenis93 <Ghjuhfvvbhe.2021!>
2025-02-01 18:07:57 +0500

    deleted a unneccecary cycle

--- group/homework/daleshin/hm_2025_01_29.py
+++ group/homework/daleshin/hm_2025_01_29.py
@@ -1,8 +1,7 @@
-test_nums: list = [12, 32, 0, -4, 4, 564564, 63, 5976]
+test_nums: list[int]  = [12, 32, 0, -4, 4, 564564, 63, 5976]
 # test_nums.sort()
 
-for i in range(len(test_nums)): # измеряю длину списка, чтобы пройти по каждому значению из списка
-    for k in range(len(test_nums) - 1): # проверяю каждый элемент списка с другими в списке. отнимаю 1, чтобы ограничить длину списка, так как у меня дальше есть выражение k+1 , при последжних итерациях выдает ошибку IndexError: list index out of range , потому что ссылается на несуществующий элемент списка
-        if test_nums[k] > test_nums[k + 1]:
-            test_nums[k], test_nums[k + 1] = test_nums[k + 1], test_nums[k] # меняю местами в списке значения
+for k in range(len(test_nums) - 1): # отнимаю 1, чтобы ограничить длину списка, так как у меня дальше есть выражение k+1 , при последжних итерациях выдает ошибку IndexError: list index out of range , потому что ссылается на несуществующий элемент списка
+    if test_nums[k] > test_nums[k + 1]:
+        test_nums[k], test_nums[k + 1] = test_nums[k + 1], test_nums[k] # меняю местами в списке значения
 print(test_nums)

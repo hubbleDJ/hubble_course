d71a590caa70df556ac53ead440d11b5488a9f6d
festo24 <festo1992@gmail.com>
2025-02-02 23:45:17 +0600

    edit homework_3, popsort

---
+++ group/homework/uvarovfesto24/2025_02_01.py
@@ -0,0 +1,9 @@
+popList: list = [2, 4, 5, 4, 7, 2, 8, 4, 9, 2]
+popLeight: int = (len(popList))
+print("sort start= ", popList)
+
+for i in range(popLeight):
+    for j in range(popLeight-1):
+        if popList[j] > popList[j+1]:
+           popList[j], popList[j+1] = popList[j+1], popList[j]
+print("sort end= ", popList)

d9ac40617b341b300aa970f3f10f479383fca96e
Julia-Krsh <krasheninnikova-y05@mail.ru>
2025-02-04 16:25:03 +0600

    created hw_2025-01-29 in the new branch (#32)
    created hw_2025-01-29

---
+++ group/homework/jekrasheninnikova/hw_2025_01_29.py
@@ -0,0 +1,11 @@
+puz_meth: list = [12, 32, 0, -4, 4, 564564]
+
+print(f'Исходные данные {puz_meth}')
+
+for i in range(len(puz_meth)):
+    for j in range(len(puz_meth) - 1):
+        if puz_meth[j] > puz_meth[j + 1]:
+            puz_meth[j], puz_meth[j + 1] = puz_meth[j + 1], puz_meth[j]
+            print(puz_meth)
+
+print(f'Итог: {puz_meth}')

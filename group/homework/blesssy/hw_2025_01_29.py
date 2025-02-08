b5edc87bd8e35cf707147fd6eab77d64961e96f4
Lesssy <aaa.lol.haaa@gmail.com>
2025-02-08 22:35:01 +0300

    Lessy/add hw 2025_01_29 (#29)
    add hw 2025_01_29

---
+++ group/homework/blesssy/hw_2025_01_29.py
@@ -0,0 +1,20 @@
+# Написать пузырьковую сортировку
+# [12, 32, 0, -4, 4, 56564]
+
+def bubble_sort(nums: list[int]) -> list[int]:
+    """sorting numbers in ascending order """
+
+    length_nums  = len(nums)
+    for ordinal_num  in range(0, length_nums - 1):
+        for current_ord_num  in range(0, length_nums - 1):
+            if nums[current_ord_num] > nums[current_ord_num + 1]:
+                nums[current_ord_num], nums[current_ord_num + 1] = \
+                        nums[current_ord_num  + 1], nums[current_ord_num]
+
+    return nums
+
+list1: list[int] = [-500, -501, 5, 12, 32, 0, -4, 4, 56564, -432]
+sorted_list = bubble_sort(list1[:])
+
+print("Отсортированный список 1: ", sorted_list)
+print("Отсортированный список 1: ", bubble_sort([-1, -5]))

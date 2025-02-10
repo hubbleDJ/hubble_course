b261145b9ad1a5eec2258c95b647e11aa8c896c5
Jkrsh <krasheninnikova-y05@mail.ru>
2025-02-10 18:11:21 +0600

    created hw_2025-02-05

--- group/homework/jekrasheninnikova/hw_2025_01_29.py
+++ group/homework/jekrasheninnikova/hw_2025_01_29.py
@@ -3,6 +3,7 @@ puz_meth: list = [12, 32, 0, -4, 4, 564564]
 print(f'Исходные данные {puz_meth}')
 
 for i in range(len(puz_meth)):
+    print(puz_meth)
     for j in range(len(puz_meth) - 1):
         if puz_meth[j] > puz_meth[j + 1]:
             puz_meth[j], puz_meth[j + 1] = puz_meth[j + 1], puz_meth[j]

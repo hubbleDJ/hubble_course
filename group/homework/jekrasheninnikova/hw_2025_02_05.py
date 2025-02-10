b5c7efc68a10ac273713d4d36acccdd0f7e3b75b
Jkrsh <krasheninnikova-y05@mail.ru>
2025-02-10 18:21:19 +0600

    created hw_2025-02-05

---
+++ group/homework/jekrasheninnikova/hw_2025_02_05.py
@@ -0,0 +1,23 @@
+# Прекратить сортировку, если массив уже отсортирован
+# Оптимизировать это в функцию
+puz_meth: list = [12, 32, 0, -4, 4, 564564]
+
+print(f'Исходные данные {puz_meth}')
+
+def puzirk_m(puz: list[int|float]) -> None:
+    """Сортирует список методом пузырька, не перепроверяя его лишний раз, если он уже отсортирован."""
+    n: int = 0
+
+    for i in range(len(puz_meth)):
+        print(puz_meth)
+        for j in range(len(puz_meth) - 1):
+            if puz_meth[j] < puz_meth[j + 1]:
+                n += 1
+                if n == len(puz_meth):
+                    exit(f'Итоговое значение: {puz_meth}')
+            elif puz_meth[j] > puz_meth[j + 1]:
+                n=0
+                puz_meth[j], puz_meth[j + 1] = puz_meth[j + 1], puz_meth[j]
+
+
+puzirk_m(puz_meth)

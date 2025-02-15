22306ff488e647c5a31042372a01dd9b3501eda8
agubanova <an.gubanova2012@yandex.ru>
2025-02-15 12:02:11 +0300

    corrected my hw :(

--- group/homework/agubanova/hw_2025_02_05.py
+++ group/homework/agubanova/hw_2025_02_05.py
@@ -1,6 +1,6 @@
 # Написать функцию, которая возвращает максимальное значение в списке
 
-def NumberMax(number_list: list[float|int]) -> float|int:
+def number_max(number_list: list[float|int]) -> float|int:
     """Находит максимальное значение"""
     
     num_max: float|int = number_list[0]
@@ -12,7 +12,7 @@ def NumberMax(number_list: list[float|int]) -> float|int:
 
 # Написать функцию, которая возвращает минимальное значение в списке
 
-def NumberMin(number_list: list[float|int]) -> float|int:
+def number_min(number_list: list[float|int]) -> float|int:
     """Находит минимальное значение"""
     
     num_min: float|int = number_list[0]
@@ -24,39 +24,36 @@ def NumberMin(number_list: list[float|int]) -> float|int:
 
 # Написать функцию, которая возвращает все четные элементы списка в виде новго списка
 
-def EvenNums(number_list: list[float|int]) -> list[float|int]:
+def even_nums(number_list: list[float|int]) -> list[float|int]:
     """Поиск четного и добавление в новый список"""
     
-    number_sorted:list[float|int] = []
+    even_numbers_list: list[float|int] = []
     for num in number_list:
-        if num % 2 == 0 and num != 0:
-            number_sorted.append(num)
+        if num % 2 == 0:
+            even_numbers_list.append(num)
 
-    return number_sorted
+    return even_numbers_list
 
-# Оптимизировать пузырьковую сортировку
-# Прекратить сортировку если сортировать уже не надо
-# Засунуть сортировку в функцию
+# Написать пузырьковую сортровку
+spisok_num: list = [12, 32, 0, -4, 4, 564564]
 
-def BubbleSort(number_list: list[float|int]) -> list[float|int]:
-    """Пузырьковая сортировка списка """
-    
+def buble_sort(number_list: list[float|int]) -> list[float|int]:
+    """Сортирует пузырьковым методом(последовательно сравнивать значения соседних элементов и меняет 
+    числа местами, если предыдущее оказывается больше последующего. 
+    Таким образом элементы с большими значениями оказываются в конце списка, 
+    а с  меньшими остаются в начале)"""
+
     sorted_done: bool = False
-
-    for passage_list in range(len(number_list) - 1):
+    for pass_num in range(len(number_list) - 1):
         sorted_done = True
-        for index_value in range(len(number_list) - 1 - passage_list):
-            if number_list[index_value] > number_list[index_value + 1]:
-                number_list[index_value], number_list[index_value + 1] = number_list[index_value + 1], number_list[index_value]
+        for compare_index in range(len(number_list) - pass_num - 1):
+            if number_list[compare_index] > number_list[compare_index + 1]:
+                number_list[compare_index], number_list[compare_index + 1] = \
+                number_list[compare_index + 1], number_list[compare_index]
                 sorted_done = False
         if sorted_done == True:
-            break
-
+            break 
+    
     return number_list
 
-number_list: list[float|int] = [1, 2, 3, 4, 5, 6]
-
-print(NumberMax(number_list))
-print(NumberMin(number_list))
-print(EvenNums(number_list))
-print(BubbleSort(number_list))
+print(number_max(spisok_num), number_min(spisok_num), even_nums(spisok_num), buble_sort(spisok_num), sep='\n')

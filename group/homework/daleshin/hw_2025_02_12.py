0af8bb7f9d7f79090c8b26760f43216b43d1f3e5
AlDenis93 <Ghjuhfvvbhe.2021!>
2025-02-16 22:35:25 +0500

    daleshin/hwm2025-02-12

---
+++ group/homework/daleshin/hw_2025_02_12.py
@@ -0,0 +1,18 @@
+"""Homework 2025-02-12"""
+# Написать рекурсивную функцию по вычислению факториала
+
+number: int = int(input("Введите число: "))
+
+def find_factorial(number: int|float) -> int|float:
+    """Ищет факториал,  
+    в случае ошибки - вернет - 1"""
+
+    if number < 1 or int(number) != number:
+        return -1
+
+    if number == 1 or number == 0:
+        return 1
+
+    return number * find_factorial(number - 1)
+
+print(find_factorial(number))

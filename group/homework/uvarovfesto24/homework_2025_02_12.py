caaf0de5a3b1c2a5e2c220d678d9ef83ffe4c40f
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-02-26 19:17:51 +0300

    hw_2025-02-12 | fix style

--- group/homework/uvarovfesto24/homework_2025_02_12.py
+++ group/homework/uvarovfesto24/homework_2025_02_12.py
@@ -1,6 +1,11 @@
-def factorial(num):
+"""Домашка по написанию факториала"""
+
+def factorial(num: int|float) -> int|float:
+    """Считаем факториал числа"""
+
     if num == 0 or num == 1:
         return 1
     else:
-        return   num * factorial(num - 1)
+        return num * factorial(num - 1)
+
 print(factorial(7))

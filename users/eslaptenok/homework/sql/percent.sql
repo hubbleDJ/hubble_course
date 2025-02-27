7b8311e5cc66cb54e81a7018d643d6352ba80510
Maksim Strometskiy <mvstrometskiy@avito.ru>
2025-02-27 21:55:51 +0300

    drop path users

--- users/eslaptenok/homework/sql/percent.sql
+++
@@ -1,5 +0,0 @@
-select 
-	ROUND((COUNT(*) - COUNT(goods_id)) * 100.00 / COUNT(*), 2) as res
-from shop_users
-left join shop_orders
-	on shop_users.id = shop_orders.buyer_id 
--- users/eslaptenok/homework/sql/top10.sql
+++
@@ -1,9 +0,0 @@
-select 
-	name,
-	count(1) as quantity
-from shop_goods
-join shop_orders
-	on shop_goods.id = shop_orders.goods_id 
-group by name
-order by quantity desc
-limit 10
